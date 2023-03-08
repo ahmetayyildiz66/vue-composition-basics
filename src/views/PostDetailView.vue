a2ee342e6bacad6eeb6692e0cb64df60fe2af7df
Ahmet Ayyildiz <ahmet.ayyildiz@qlik.com>
2023-03-08 08:26:16 +0100

    vue router: useRoute composable

---
+++ src/views/PostDetailView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="post-detail">
+    <h1>This is a post page</h1>
+    <p>Display the content of post with ID of {{ $route.params.id }}</p>
+    <div>
+      <button @click="showPostId">Show Post ID</button>
+    </div>
+    <p>
+      <router-link to="/posts">&lt; Back</router-link>
+    </p>
+  </div>
+</template>
+
+<script setup>
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const showPostId = () => {
+  alert(`The ID of this post is: ${route.params.id}`);
+};
+</script>
