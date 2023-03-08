e6969b744ed94d67989faa1af7f1bacbd880c991
Ahmet Ayyildiz <ahmet.ayyildiz@qlik.com>
2023-03-08 16:53:49 +0100

    ref: using template refs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
-    <h2>{{ appTitle }}</h2>
+    <h2 ref="appTitleRef">{{ appTitle }}</h2>
+
     <h3>{{ counterData.title }}</h3>
 
     <div class="btns">
@@ -22,7 +23,7 @@
 /*
   imports
 */
-import { reactive, computed, watch, onMounted } from "vue";
+import { ref, reactive, computed, watch, onMounted } from "vue";
 
 import { vAutofocus } from "@/directives/vAutofocus";
 
@@ -30,8 +31,10 @@ import { vAutofocus } from "@/directives/vAutofocus";
 
 const appTitle = "My Amazing Counter App";
 
+const appTitleRef = ref(null);
+
 onMounted(() => {
-  console.log("Do stuff related to App Title");
+  console.log(`The app title is ${appTitleRef.value.offsetWidth}`);
 });
 
 /* counter */
