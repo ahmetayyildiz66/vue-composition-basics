d45d8bcce96378b3769121aaa4fe877a70c7cf8b
Ahmet Ayyildiz <ahmet.ayyildiz@qlik.com>
2023-03-08 17:19:45 +0100

    slots: using slots

---
+++ src/components/Modal.vue
@@ -0,0 +1,32 @@
+<template>
+  <teleport to=".modals-container">
+    <div class="modal">
+      <h1><slot name="title" /></h1>
+      <slot />
+      <pre>{{ slots.title }}</pre>
+      <button>Hide modal</button>
+    </div>
+  </teleport>
+</template>
+
+<script setup>
+import { useSlots } from "vue";
+
+const slots = useSlots();
+
+console.log(slots.title());
+</script>
+
+<style scoped>
+.modal {
+  background: beige;
+  color: darkgray;
+  padding: 10px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+}
+</style>
