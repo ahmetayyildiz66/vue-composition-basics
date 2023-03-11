bbccd68f9b8f7e8581069b47ce8815f154e8ba3e
Ahmet Ayyildiz <ahmet.ayyildiz@qlik.com>
2023-03-11 12:44:58 +0100

    state management: pinia usage

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,12 +1,28 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
+export const useCounterStore = defineStore("counter", () => {
+  const count = ref(0);
+  const doubleCount = computed(() => count.value * 2);
   function increment() {
-    count.value++
+    count.value++;
   }
 
-  return { count, doubleCount, increment }
-})
+  return { count, doubleCount, increment };
+});
+
+// It also has options API version
+// export const useCounterStore = defineStore({
+//   id: "counter",
+//   state: () => ({
+//     counter: 0,
+//   }),
+//   getters: {
+//     doubleCount: (state) => state.counter * 2,
+//   },
+//   actions: {
+//     increment() {
+//       this.counter++;
+//     },
+//   },
+// });
